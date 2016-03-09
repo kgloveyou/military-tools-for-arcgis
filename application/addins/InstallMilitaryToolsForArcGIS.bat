e03031e3857c20ffe165f971a858c61b64a2e0ad
Joseph McCausland <jmccausland@esri.com>
2016-03-09 09:27:36 -0500

    Update batch file to work with x86 and non x86 folders

--- application/addins/InstallMilitaryToolsForArcGIS.bat
+++ application/addins/InstallMilitaryToolsForArcGIS.bat
@@ -1,5 +1,12 @@
-@echo off
+C:
+IF NOT EXIST "C:\Program Files (x86)\Common Files\ArcGIS\bin" GOTO NOX86DIR
 "C:\Program Files (x86)\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" /s ArcMapAddinCoordinateTool.esriAddIn
 "C:\Program Files (x86)\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" /s ArcMapAddinGeodesyAndRange.esriAddIn
 "C:\Program Files (x86)\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" /s ArcMapAddinVisibility.esriAddIn
 start "C:\Program Files (x86)\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" ArcMapAddinMAToolbar.esriAddIn
+exit
+:NOX86DIR
+"C:\Program Files\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" /s ArcMapAddinCoordinateTool.esriAddIn
+"C:\Program Files\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" /s ArcMapAddinGeodesyAndRange.esriAddIn
+"C:\Program Files\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" /s ArcMapAddinVisibility.esriAddIn
+start "C:\Program Files\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" ArcMapAddinMAToolbar.esriAddIn
