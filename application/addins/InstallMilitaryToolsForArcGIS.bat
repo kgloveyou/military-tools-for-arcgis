802bd618d52d48d33e2488d0726a88cfb6869f80
Joseph McCausland <jmccausland@esri.com>
2016-03-17 08:49:16 -0400

    update install batch file

--- application/addins/InstallMilitaryToolsForArcGIS.bat
+++ application/addins/InstallMilitaryToolsForArcGIS.bat
@@ -1,12 +1,7 @@
 C:
-IF NOT EXIST "C:\Program Files (x86)\Common Files\ArcGIS\bin" GOTO NOX86DIR
-"C:\Program Files (x86)\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" /s ArcMapAddinCoordinateTool.esriAddIn
-"C:\Program Files (x86)\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" /s ArcMapAddinGeodesyAndRange.esriAddIn
-"C:\Program Files (x86)\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" /s ArcMapAddinVisibility.esriAddIn
-start "C:\Program Files (x86)\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" ArcMapAddinMAToolbar.esriAddIn
-exit
-:NOX86DIR
-"C:\Program Files\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" /s ArcMapAddinCoordinateTool.esriAddIn
-"C:\Program Files\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" /s ArcMapAddinGeodesyAndRange.esriAddIn
-"C:\Program Files\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" /s ArcMapAddinVisibility.esriAddIn
-start "C:\Program Files\Common Files\ArcGIS\bin\ESRIRegAddIn.exe" ArcMapAddinMAToolbar.esriAddIn
+SET COMMONDIR="C:\Program Files (x86)\Common Files\ArcGIS\bin"
+IF NOT EXIST %COMMONDIR% SET COMMONDIR="C:\Program Files\Common Files\ArcGIS\bin"
+%COMMONDIR%\ESRIRegAddIn.exe /s ArcMapAddinCoordinateTool.esriAddIn
+%COMMONDIR%\ESRIRegAddIn.exe /s ArcMapAddinGeodesyAndRange.esriAddIn
+%COMMONDIR%\ESRIRegAddIn.exe /s ArcMapAddinVisibility.esriAddIn
+start %COMMONDIR%\ESRIRegAddIn.exe ArcMapAddinMAToolbar.esriAddIn
